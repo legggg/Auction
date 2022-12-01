dcd8a2f7178cdd84ed5216315d29dd637197a5f6
legggg <dys02143@naver.com>
2022-12-01 15:38:31 +0900

    message

---
+++ src/main/java/com/example/k/KApplication.java
@@ -0,0 +1,13 @@
+package com.example.k;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KApplication.class, args);
+	}
+
+}
